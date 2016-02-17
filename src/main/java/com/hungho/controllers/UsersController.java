a18f682668aa39bd4270a2037eecec3fd6a12260
Chen Xuan <chancore@sina.com>
2016-02-17 15:35:52 +0800

    A

--- src/main/java/com/hungho/controllers/UsersController.java
+++ src/main/java/com/hungho/controllers/UsersController.java
@@ -1,8 +1,16 @@
 package com.hungho.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value="/user")
 public class UsersController {
 	
+	
+	@RequestMapping(value="/sys_login")
+	public String index(){
+		
+		return "index";
+	}
 }
