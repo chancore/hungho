f1bd8f972435c60ac053a8747c6483a516c77f50
Chen Xuan <chancore@sina.com>
2016-02-17 14:09:31 +0800

    A

--- src/main/java/com/hungho/controllers/UsersController.java
+++ src/main/java/com/hungho/controllers/UsersController.java
@@ -1,5 +1,8 @@
 package com.hungho.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UsersController {
 
 }
---
+++ src/main/java/com/hungho/model/Users.java
@@ -0,0 +1,18 @@
+package com.hungho.model;
+
+
+public class Users {
+
+	
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+}
